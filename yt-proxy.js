9dec64e4145353a473ed6f3a18bb3b6da94070ba
Shkamp <leo.gombac@gmail.com>
2020-02-02 20:26:10 +0100

    removed unnecessary code

--- yt-proxy.js
+++ yt-proxy.js
@@ -20,15 +20,4 @@ http.createServer(function (req, res) {
 }).listen(8080);
 var returnUrl = "";
 
-// Optional arguments passed to youtube-dl.
-var options = ['--format=18'];
-function getUrl(url) {
-    youtubedl.getInfo(url, options, function (err, info) {
-        if (err) throw err;
-        res = info;
-        console.log(info.url)
-        console.log(res);
-        returnUrl = info.url;
-    })
-
 }
